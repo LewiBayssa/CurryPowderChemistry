78230212f005e58fad8b8bfad33bed02cf5aedf4
Downbeatfoil <124329022+Downbeatfoil@users.noreply.github.com>
2023-10-29 19:10:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,64 @@ document.addEventListener('scroll', function () {
         myBar.style.width = scrolled + "%";
     }
 });
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.getElementById('toggle-button');
+    const eligibilityContainer = document.getElementById('eligibility-container');
+
+    toggleButton.addEventListener('click', function () {
+        // Toggle the visibility of the eligibility container
+        if (eligibilityContainer.style.display === 'none' || eligibilityContainer.style.display === '') {
+            eligibilityContainer.style.display = 'block';
+        } else {
+            eligibilityContainer.style.display = 'none';
+        }
+    });
+});
+document.addEventListener('DOMContentLoaded', function () {
+    const discountedTaxesElement = document.getElementById('discounted-taxes');
+    const initialAmountInput = document.getElementById('initial-amount');
+    const calculateDiscountButton = document.getElementById('calculate-discount');
+
+// Define a function to calculate and display the discounted taxes
+    function calculateAndDisplayDiscount() {
+    // Get the user's input from the input element
+        const userInput = initialAmountInput.value;
+
+    // Check if the user input is a valid number
+        if (!isNaN(userInput)) {
+        // Convert the user input to a number
+            const initialTaxAmount = parseFloat(userInput);
+
+        // Calculate the 30% discount
+            let discountedTaxAmount = initialTaxAmount;
+
+        // Set the initial display value
+            discountedTaxesElement.textContent = `$${discountedTaxAmount.toFixed(2)}`;
+
+        // Define a function to update and display the ticking number
+        function updateTickingNumber() {
+            if (discountedTaxAmount > 0.72 * initialTaxAmount) {
+                discountedTaxAmount -= 1;
+            } else if (discountedTaxAmount > 0.7028 * initialTaxAmount) {
+                discountedTaxAmount -= 0.1;
+            } else if (discountedTaxAmount > 0.70 * initialTaxAmount) {
+                discountedTaxAmount -= 0.01;
+            } else {
+                clearInterval(tickingInterval);
+            }
+            discountedTaxesElement.textContent = `$${discountedTaxAmount.toFixed(2)}`;
+        }
+        
+        
+
+        // Update the ticking number every 1000 milliseconds (1 second)
+            const tickingInterval = setInterval(updateTickingNumber, 2);
+        } else {
+            alert('Invalid input. Please enter a valid number.');
+        }
+    }
+
+// Add a click event listener to the button
+    calculateDiscountButton.addEventListener('click', calculateAndDisplayDiscount);
+});
+
